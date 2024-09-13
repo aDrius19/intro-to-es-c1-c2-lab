567c7217259e0e3e95e1c86323cb09943f0f33e1
Darius Loga <dariusloga09@gmail.com>
2024-09-13 07:05:19 +0200

    refactored

--- day1_part2.c
+++ day1_part2.c
@@ -19,56 +19,25 @@ int main()
         {
             char var[MAX_ARRAY_VAL] = "";
             char calibValLine[MAX_ARRAY_VAL] = "";
-            char c1[MAX_ARRAY_VAL] = "";
-            char *calibTextValLine[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; //harcoded values to use as substring
-            printf("\n%s", oneFileLine);
+            char *calibTextValLine[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; // harcoded values to use as substring
 
             for (int i = 0; oneFileLine[i] != '\0'; i++)
             {
-                char ch = oneFileLine[i];
-                c1[i] = oneFileLine[i];
+                // check for values "one" through "nine"s
+                for (int j = 0; j < 9; j++) 
+                {
+                    int wordLen = strlen(calibTextValLine[j]);
 
-                if (strstr(c1, calibTextValLine[0]) != NULL) //a little boilerplate, unfortunately, for finding the occurence of the "digits"
-                {
-                    strcat(var, "1"); //add the corresponding digit to the var array
-                    memset(c1, ' ', sizeof(c1));
-                }else if ( strstr(c1, calibTextValLine[1]) != NULL) 
-                {
-                    strcat(var, "2");
-                    memset(c1, ' ', sizeof(c1));
-                }else if ( strstr(c1, calibTextValLine[2]) != NULL) 
-                {
-                    strcat(var, "3");
-                    memset(c1, ' ', sizeof(c1));
-                }else if ( strstr(c1, calibTextValLine[3]) != NULL) 
-                {
-                    strcat(var, "4");
-                    memset(c1, ' ', sizeof(c1));
-                }else if ( strstr(c1, calibTextValLine[4]) != NULL) 
-                {
-                    strcat(var, "5");
-                    memset(c1, ' ', sizeof(c1));
-                }else if ( strstr(c1, calibTextValLine[5]) != NULL) 
-                {
-                    strcat(var, "6");
-                    memset(c1, ' ', sizeof(c1));
-                }else if ( strstr(c1, calibTextValLine[6]) != NULL) 
-                {
-                    strcat(var, "7");
-                    memset(c1, ' ', sizeof(c1));
-                }else if ( strstr(c1, calibTextValLine[7]) != NULL) 
-                {
-                    strcat(var, "8");
-                    memset(c1, ' ', sizeof(c1));
-                }else if ( strstr(c1, calibTextValLine[8]) != NULL) 
-                {
-                    strcat(var, "9");
-                    memset(c1, ' ', sizeof(c1));
+                    // check if the substring matches and can handle the overlapping values
+                    if (strncmp(&oneFileLine[i], calibTextValLine[j], wordLen) == 0) 
+                    {
+                        strcat(var, (char[]){'1' + j, '\0'}); // concat the corresponding digit
+                    }
                 }
 
-                if (isdigit(ch))
+                if (isdigit(oneFileLine[i]))
                 {
-                    strncat(var, &ch, 1);
+                    strncat(var, &oneFileLine[i], 1);
                 }
             }
 
@@ -76,7 +45,6 @@ int main()
             calibValLine[1] = var[strlen(var) - 1];
 
             calibValSum += atoi(calibValLine);
-            printf("%s\n", calibValLine);
         }
         fclose(inputFile);
     }
